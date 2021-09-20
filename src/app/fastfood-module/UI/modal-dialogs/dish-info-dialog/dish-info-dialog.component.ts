505e49df30d225247e38a772af45a4fa02da5d10
JuliaClifford <69593248+JuliaClifford@users.noreply.github.com>
2021-09-21 01:03:16 +0300

    All dish category view added, store created

---
+++ src/app/fastfood-module/UI/modal-dialogs/dish-info-dialog/dish-info-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dish-info-dialog',
+  templateUrl: './dish-info-dialog.component.html',
+  styleUrls: ['./dish-info-dialog.component.scss']
+})
+export class DishInfoDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
